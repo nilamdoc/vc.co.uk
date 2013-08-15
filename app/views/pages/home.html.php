cb81210e265791fd5a1ff882e9ddb89d6e88c0a8
Nilam Doctor <nilamdoc@gmail.com>
2013-08-15 13:38:39 +0530

    footer

---
+++ app/views/pages/home.html.php
@@ -0,0 +1,15 @@
+<div class="well">
+<h3>in bitcoin we trust</h3>
+<blockquote>"The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous 
+centuries of its existence." <small>Nikola Tesla</small></blockquote>
+<p>Nikola Tesla One wonders how applicable Bitcoin is to Nikola Tesla's statement. Here at IBWT we plan to help put it to the test, by enabling you, our customers, to trade your Bitcoin for sterling with each other in a secure and trusted environment.</p>
+<ul>
+    <li>Fees are 0.8% per transaction</li>
+    <li>Simple verification means you could be a full customer in a matter of days</li>
+    <li>Security ensured with Cold Storage, SSL 256bit encryption & 2FA</li>
+    <li>Dedicated Server for an enhanced customer experience</li>
+    <li>Deposits & Withdrawals via wire transfers</li>
+    <li>Services only available to UK residents</li>
+    <li>Based and registered within the UK to help build your trust</li>
+</ul>
+</div>
