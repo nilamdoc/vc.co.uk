92b9dab43354e92f66aa8301385b07abbac64475
Nilam Doctor <nilamdoc@gmail.com>
2013-08-23 14:32:49 +0530

    totp

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -37,7 +37,8 @@ class SessionsController extends \lithium\action\Controller {
 							Session::delete('default');
 						}else{
 							$checkResult = $ga->verifyCode($details['secret'], $totp, 2);		
-							if ($checkResult) {
+							
+							if ($checkResult==1) {
 								Session::write('default',$default);
 								$user = Session::read('default');
 								return $this->redirect('ex::dashboard');
