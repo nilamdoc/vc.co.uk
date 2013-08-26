6d019431106fc467561ae175ef39c28fc6b4b062
Nilam Doctor <nilamdoc@gmail.com>
2013-08-26 20:15:01 +0530

    remove

--- app/views/news/launching.html.php
+++ app/views/news/launching.html.php
@@ -2,7 +2,6 @@
 <h3>Launching 1st September 2013</h3>
 <ul>
 <li>We are going public with the beta version on 1st September 2013. </li>
-<li>All users verifing themselves before 15th September 2013 will get a deposit of 0.1 BTC to start trading with bitcoins.</li>
 </ul>
 <br>
 <br>
