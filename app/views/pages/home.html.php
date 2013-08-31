1902318be310b9410251ce0a0d37e131a7f44ae8
Nilam Doctor <nilamdoc@gmail.com>
2013-08-31 08:06:05 +0530

    home

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -27,7 +27,7 @@ centuries of its existence." <small>Nikola Tesla</small></blockquote>
 </ul>
 <h3>News</h3>
 <ul>
-	<li><a href="/news">Please find our recent up to date news via our news</a>.</li>
+	<li>Please find our recent news <a href="/news">here.</a>.</li>
 </ul>
 <h3>Find us</h3>
 <ul class="unstyled">
