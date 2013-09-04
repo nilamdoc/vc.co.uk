40c61a6adeeace90d127bfe94fd883b0520cd8fa
Nilam Doctor <nilamdoc@gmail.com>
2013-09-04 14:04:17 +0530

    All files again!

--- app/extensions/command/Walletnotify.php
+++ app/extensions/command/Walletnotify.php
@@ -7,12 +7,12 @@ use app\extensions\action\Bitcoin;
 
 
 class Walletnotify extends \lithium\console\Command {
-
     public function index($s=null) {
 			$bitcoin = new Bitcoin('http://'.BITCOIN_WALLET_SERVER.':'.BITCOIN_WALLET_PORT,BITCOIN_WALLET_USERNAME,BITCOIN_WALLET_PASSWORD);
+
 		$getrawtransaction = $bitcoin->getrawtransaction($s);
 		$decoderawtransaction = $bitcoin->decoderawtransaction($getrawtransaction);		
-//		print_r($decoderawtransaction);
+
 			foreach($decoderawtransaction['vout'] as $out){
 				foreach($out['scriptPubKey']['addresses'] as $address){
 					$Amount = (float)$out['value'];
@@ -25,22 +25,41 @@ class Walletnotify extends \lithium\console\Command {
 							'DateTime' => new \MongoDate(),
 							'TransactionHash' => $s,
 							'username' => $details['username'],
-							'Amount'=> $Amount
+							'address'=>$address,
+							'Amount'=> $Amount,
+							'Added'=>false
 						);
-						$Transactions = Transactions::create();		
-						$Transactions->save($data);
-						$data = array(
-							'balance.BTC' => (float)$details['balance.BTC'] + $Amount,
-						);
-//						print_r($data);
-						$details = Details::find('all',
-							array(
-									'conditions'=>array('bitcoinaddress'=>$address)
-								))->save($data);
+						$Transactions = Transactions::find('first',array(
+							'conditions'=>array('TransactionHash' => $s)
+						));
+						if($Transactions['_id']==""){
+							$t = Transactions::create();
+							$data = array(
+								'DateTime' => new \MongoDate(),
+								'TransactionHash' => $s,
+								'username' => $details['username'],
+								'address'=>$address,							
+								'Amount'=> $Amount,
+								'Added'=>true
+							);							
+							$t->save($data);
+							
+							$dataDetails = array(
+									'balance.BTC' => (float)$details['balance.BTC'] + $Amount,
+								);
+						
+							$details = Details::find('all',
+								array(
+										'conditions'=>array('bitcoinaddress'=>$address)
+									))->save($dataDetails);
+						}else{
+							$Transactions = Transactions::find('first',array(
+								'conditions'=>array('TransactionHash' => $s)
+							))->save($data);
+						}
 					}
 				}
 			}
-
 		}
 }
 ?>
