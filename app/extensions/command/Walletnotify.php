7f4791ed5422376b705c947f9f5a692c07d71898
Nilam Doctor <nilamdoc@gmail.com>
2013-09-13 15:54:11 +0530

    Transfer to MyWallet

--- app/extensions/command/Walletnotify.php
+++ app/extensions/command/Walletnotify.php
@@ -3,12 +3,15 @@ namespace app\extensions\command;
 
 use app\models\Transactions;
 use app\models\Details;
-use app\extensions\action\Bitcoin;
+use app\models\Parameters;
 
+use app\extensions\action\Bitcoin;
 
 class Walletnotify extends \lithium\console\Command {
     public function index($s=null) {
 			$bitcoin = new Bitcoin('http://'.BITCOIN_WALLET_SERVER.':'.BITCOIN_WALLET_PORT,BITCOIN_WALLET_USERNAME,BITCOIN_WALLET_PASSWORD);
+			$paytxfee = Parameters::find('first');
+			$txfee = $paytxfee['paytxfee'];
 
 		$getrawtransaction = $bitcoin->getrawtransaction($s);
 		$decoderawtransaction = $bitcoin->decoderawtransaction($getrawtransaction);		
@@ -26,7 +29,7 @@ class Walletnotify extends \lithium\console\Command {
 							'TransactionHash' => $s,
 							'username' => $details['username'],
 							'address'=>$address,
-							'Amount'=> $Amount,
+							'Amount'=> $Amount - (float)$txfee,
 							'Added'=>false
 						);
 						$Transactions = Transactions::find('first',array(
@@ -34,24 +37,35 @@ class Walletnotify extends \lithium\console\Command {
 						));
 						if($Transactions['_id']==""){
 							$t = Transactions::create();
+							$comment = "User: ".$details['username']."; Address: ".$address."; Amount:".$Amount.";";
+							$transfer = $bitcoin->sendfrom($details['username'], BITCOIN_ADDRESS, (float)$Amount,(int)0,$comment);
+
+							if(isset($transfer['error'])){
+								$error = $transfer['error']; 
+							}else{
+								$error = $transfer;
+							}
+							
 							$data = array(
 								'DateTime' => new \MongoDate(),
 								'TransactionHash' => $s,
 								'username' => $details['username'],
 								'address'=>$address,							
-								'Amount'=> $Amount,
-								'Added'=>true
+								'Amount'=> $Amount - (float)$txfee,
+								'Added'=>true,
+								'Transfer'=>$error,
 							);							
 							$t->save($data);
 							
 							$dataDetails = array(
-									'balance.BTC' => (float)$details['balance.BTC'] + $Amount,
+									'balance.BTC' => (float)$details['balance.BTC'] + $Amount - (float)$txfee,
 								);
 						
 							$details = Details::find('all',
 								array(
 										'conditions'=>array('bitcoinaddress'=>$address)
 									))->save($dataDetails);
+
 						}else{
 							$Transactions = Transactions::find('first',array(
 								'conditions'=>array('TransactionHash' => $s)
