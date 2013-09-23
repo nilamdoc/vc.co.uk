62de9ca313eece96f70a39cda1ec098cd97d8b09
Nilam Doctor <nilamdoc@gmail.com>
2013-09-23 13:28:14 +0530

    10

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -883,7 +883,7 @@ class AdminController extends \lithium\action\Controller {
 			
 		$mongodb = Connections::get('default')->connection;
 	  $pagination = new Pagination($mongodb, '/Admin/user/{{PAGE}}/');
-		$itemsPerPage   = 20;
+		$itemsPerPage   = 10;
 		$currentPage    = $page;
 		$pagination->setQuery(array(
 			'#collection'	=>  'users',
