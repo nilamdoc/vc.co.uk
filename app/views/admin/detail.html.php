bc063ba3205c9660f12893ef5b1ba5ec022aa37f
Nilam Doctor <nilamdoc@gmail.com>
2013-09-29 18:12:35 +0530

    multiply

--- app/views/admin/detail.html.php
+++ app/views/admin/detail.html.php
@@ -29,9 +29,9 @@
 <?php foreach($details as $dt){?>
 	<tr>
 		<td style="text-align:center "><?=number_format($dt['balance.BTC'],8)?></td>
-		<td style="text-align:center "><?=number_format($dt['balance.USD'],2)?></td>
-		<td style="text-align:center "><?=number_format($dt['balance.GBP'],2)?></td>
-		<td style="text-align:center "><?=number_format($dt['balance.EUR'],2)?></td>
+		<td style="text-align:center "><?=number_format($dt['balance.USD'],4)?></td>
+		<td style="text-align:center "><?=number_format($dt['balance.GBP'],4)?></td>
+		<td style="text-align:center "><?=number_format($dt['balance.EUR'],4)?></td>
 		<td style="text-align:center "><?=$dt['email.verified']?></td>
 	</tr>
 <?php  }?>
@@ -58,40 +58,40 @@
 		</td>				
 	</tr>
 <?php  }?>
-
 	</tbody>
-	</table>
+</table>
+<hr>
 <div class="row">
 	<div class="span5">
 		<div class="navbar">
 			<div class="navbar-inner">
 			<a class="brand" href="#"><?=$t('Transactions in BTC')?> </a>
 			</div>
-		<table class="table table-condensed table-bordered table-hover" >
-		<thead>
-			<tr>
-				<th>Date</th>
-				<th>Amount BTC</th>
-				<th>Status</th>
+			<table class="table table-condensed table-bordered table-hover" >
+			<thead>
+				<tr>
+					<th>Date</th>
+					<th>Amount BTC</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($transactions as $tx){?>
+			<tr <?php ?> style="background-color:#669933 "></tr>
+				<td><?=gmdate('Y-M-d H:i:s',$tx['DateTime']->sec)?></td>
+				<td style="text-align:right "><?=number_format($tx['Amount'],8)?></td>
+				<td><?php if($tx['Added']==true){echo "Deposit";}else{echo "Withdraw";}?></td>
 			</tr>
-		</thead>
-		<tbody>
-<?php foreach ($transactions as $tx){?>
-		<tr <?php ?> style="background-color:#669933 "></tr>
-			<td><?=gmdate('Y-M-d H:i:s',$tx['DateTime']->sec)?></td>
-			<td style="text-align:right "><?=number_format($tx['Amount'],8)?></td>
-			<td><?php if($tx['Added']==true){echo "Deposit";}else{echo "Withdraw";}?></td>
-		</tr>
-<?php 
-	$Amount = $Amount + number_format($tx['Amount'],8);
-} ?>
-		<tr>
-			<th >Total</th>
-			<td style="text-align:right "><?=number_format($Amount,8)?></td>
-			<td></td>
-		</tr>
-		</tbody>
-	</table>
+			<?php 
+			$Amount = $Amount + number_format($tx['Amount'],8);
+			} ?>
+			<tr>
+				<th >Total</th>
+				<td style="text-align:right "><?=number_format($Amount,8)?></td>
+				<td></td>
+			</tr>
+			</tbody>
+			</table>
 		</div>
 	</div>
 	<div class="span5">
@@ -124,39 +124,45 @@ foreach ($Fiattransactions as $tx){?>
 } ?>
 		</tbody>
 	</table>
+		</div>
 	</div>
 </div>
-<div class="span5">
-		<div class="navbar">
+<hr>
+
+<div class="row">	
+	<div class="span5">
+	<div class="navbar">
 			<div class="navbar-inner">
 			<a class="brand" href="#"><?=$t('Pending Orders')?> </a>
 			</div>
-		<table class="table table-condensed table-bordered table-hover" >
-		<thead>
-			<tr>
-				<th style="text-align:center "><?=$t('Exchange')?></th>
-				<th style="text-align:center "><?=$t('Price')?></th>
-				<th style="text-align:center "><?=$t('Amount')?></th>
-			</tr>
-		</thead>
-		<tbody>
-				<?php foreach($UserOrders as $YO){ ?>
-					<tr>
-						<td style="text-align:left ">
-						<a href="/ex/RemoveOrder/<?=String::hash($YO['_id'])?>/<?=$YO['_id']?>/<?=$sel_curr?>" title="Remove this order">
-							<i class="icon-remove"></i></a> &nbsp; 
-						<?=$YO['Action']?> <?=$YO['FirstCurrency']?>/<?=$YO['SecondCurrency']?></td>
-						<td style="text-align:right "><?=number_format($YO['PerPrice'],3)?>...</td>
-						<td style="text-align:right "><?=number_format($YO['Amount'],3)?>...</td>
+			<table class="table table-condensed table-bordered table-hover" >
+			<thead>
+				<tr>
+					<th style="text-align:center "><?=$t('Exchange')?></th>
+					<th style="text-align:center "><?=$t('Price')?></th>
+					<th style="text-align:center "><?=$t('Amount')?></th>
+					<th style="text-align:center "><?=$t('Total')?></th>					
+				</tr>
+			</thead>
+			<tbody>
+					<?php foreach($UserOrders as $YO){ ?>
+						<tr>
+							<td style="text-align:left ">
+							<a href="/ex/RemoveOrder/<?=String::hash($YO['_id'])?>/<?=$YO['_id']?>/<?=$sel_curr?>" title="Remove this order">
+								<i class="icon-remove"></i></a> &nbsp; 
+							<?=$YO['Action']?> <?=$YO['FirstCurrency']?>/<?=$YO['SecondCurrency']?></td>
+							<td style="text-align:right "><?=number_format($YO['PerPrice'],5)?>...</td>
+							<td style="text-align:right "><?=number_format($YO['Amount'],4)?>...</td>
+							<td style="text-align:right "><?=number_format($YO['PerPrice']*$YO['Amount'],4)?>...</td>
 
-					</tr>
-				<?php }?>					
-		</tbody>
-	</table>
-		</div>
-	</div>	
-			<div class="span5"  style="height:334px;">
-			<div class="navbar">
+						</tr>
+					<?php }?>					
+			</tbody>
+			</table>
+		</div>	
+	</div>
+	<div class="span5">
+		<div class="navbar">
 				<div class="navbar-inner">
 				<a class="brand" href="#"><?=$t('Completed orders')?></a>
 				</div>
@@ -167,6 +173,7 @@ foreach ($Fiattransactions as $tx){?>
 						<th style="text-align:center "><?=$t('Exchange')?></th>
 						<th style="text-align:center "><?=$t('Price')?></th>
 						<th style="text-align:center "><?=$t('Amount')?></th>
+						<th style="text-align:center "><?=$t('Commission')?></th>						
 					</tr>
 				</thead>
 				<tbody>
@@ -176,13 +183,95 @@ foreach ($Fiattransactions as $tx){?>
 					<?=number_format($YO['PerPrice'],8)?> on <?=gmdate('Y-m-d H:i:s',$YO['DateTime']->sec)?> from <?=$YO['Transact.username']?>">
 						<td style="text-align:left ">
 						<?=$YO['Action']?> <?=$YO['FirstCurrency']?>/<?=$YO['SecondCurrency']?></td>
-						<td style="text-align:right "><?=number_format($YO['PerPrice'],3)?>...</td>
-						<td style="text-align:right "><?=number_format($YO['Amount'],3)?>...</td>
+						<td style="text-align:right "><?=number_format($YO['PerPrice'],4)?>...</td>
+						<td style="text-align:right "><?=number_format($YO['Amount'],4)?>...</td>
+						<td style="text-align:right "><?=number_format($YO['Commission.Amount'],4)?> <?=$YO['Commission.Currency']?></td>						
 					</tr>
 				<?php }?>					
 				</tbody>
 			</table>
 				</div>
-			</div>
+			</div>	
+	
+	
+	</div>
 
+</div>
+<hr>
+<div class="row">
+	<div class="span11">
+		<div class="navbar">
+			<div class="navbar-inner">
+			<a class="brand" href="#"><?=$t('Reconciliation')?> </a>
+			</div>	
+			<table class="table table-condensed table-bordered table-hover" >
+			<thead>
+				<tr>
+					<th>Summary</th>
+					<th>BTC</th>
+					<th>USD</th>
+					<th>EUR</th>
+					<th>GBP</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Opening Balance</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Current Balance</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Pending Buy Orders</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Pending Sell Orders</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>After Execution</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Commission</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Completed Buy Orders</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Completed Sell Orders</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th>Completed Order Commission</th>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				
+			</tbody>
+			</table>
+		</div>
+	</div>
 </div>
