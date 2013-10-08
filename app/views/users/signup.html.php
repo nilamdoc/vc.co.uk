e8653e19bbf65375e9198ca92ee9b7a33997d02f
Nilam Doctor <nilamdoc@gmail.com>
2013-10-08 10:39:07 +0530

    username

--- app/views/users/signup.html.php
+++ app/views/users/signup.html.php
@@ -14,7 +14,7 @@
 		<?=$this->form->field('firstname', array('label'=>'First Name','placeholder'=>'First Name' )); ?>
 		<?=$this->form->field('lastname', array('label'=>'Last Name','placeholder'=>'Last Name' )); ?>
 		<?=$this->form->field('username', array('label'=>'Username','placeholder'=>'username' )); ?>
-		
+		<p class="label label-important">Only characters and numbers, NO SPACES</p>
 		<?=$this->form->field('email', array('label'=>'Email','placeholder'=>'name@youremail.com' )); ?>
 		<?=$this->form->field('password', array('type' => 'password', 'label'=>'Password','placeholder'=>'Password' )); ?>
 		<?=$this->form->field('password2', array('type' => 'password', 'label'=>'Password Verification','placeholder'=>'same as above' )); ?>
