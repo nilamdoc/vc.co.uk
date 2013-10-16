531df50a95e019ee1aae04b5d0a1ca03c88ac149
Nilam Doctor <nilamdoc@gmail.com>
2013-10-16 14:59:13 +0530

    error

--- app/config/bootstrap/errors.php
+++ app/config/bootstrap/errors.php
@@ -9,6 +9,11 @@
 use lithium\core\ErrorHandler;
 use lithium\action\Response;
 use lithium\net\http\Media;
+use \lithium\template\View;
+use \Swift_MailTransport;
+use \Swift_Mailer;
+use \Swift_Message;
+use \Swift_Attachment;
 
 ErrorHandler::apply('lithium\action\Dispatcher::run', array(), function($info, $params) {
 	$response = new Response(array(
@@ -23,6 +28,36 @@ ErrorHandler::apply('lithium\action\Dispatcher::run', array(), function($info, $
 		'layout' => 'error',
 		'request' => $params['request']
 	));
+
+	$view  = new View(array(
+			'loader' => 'File',
+			'renderer' => 'File',
+			'paths' => array(
+				'template' => '{:library}/views/{:controller}/{:template}.{:type}.php'
+			)
+		));
+		$email = 'nilam@ibwt.co.uk';
+			$body = $view->render(
+				'template',
+				compact('response'),
+				array(
+					'controller' => 'users',
+					'template'=>'error',
+					'type' => 'mail',
+					'layout' => false
+				)
+			);
+
+			$transport = Swift_MailTransport::newInstance();
+			$mailer = Swift_Mailer::newInstance($transport);
+	
+			$message = Swift_Message::newInstance();
+			$message->setSubject("ERROR ".COMPANY_URL);
+			$message->setFrom(array(NOREPLY => 'ERROR '.COMPANY_URL));
+			$message->setTo($email);
+			$message->setBody($body,'text/html');
+			$mailer->send($message);
+
 	return $response;
 });
 
