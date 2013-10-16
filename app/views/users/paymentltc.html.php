eac1ca3a53bd9ffa64d18ab1ec5020b020cdd10d
Nilam Doctor <nilamdoc@gmail.com>
2013-10-16 16:03:34 +0530

    comm

---
+++ app/views/users/paymentltc.html.php
@@ -0,0 +1,7 @@
+<?php ?>
+<h4>Payment successfully sent!</h4>
+<p><?php 
+print_r($message);	
+?></p>
+<a href="/users/funding_ltc" class="btn btn-primary">Withdraw funds</a>
+<a href="/users/transactions" class="btn btn-primary">Transactions</a>
