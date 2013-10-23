6a642a02641cb50e0074da7de4a81a0d4027e8f8
Nilam Doctor <nilamdoc@gmail.com>
2013-10-23 18:12:05 +0530

    graph

--- app/views/layouts/error.html.php
+++ app/views/layouts/error.html.php
@@ -1,3 +1,38 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2012, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+/**
+ * This layout is used to render error pages in both development and production. It is recommended
+ * that you maintain a separate, simplified layout for rendering errors that does not involve any
+ * complex logic or dynamic data, which could potentially trigger recursive errors.
+ */
+?><!doctype html>
+<html>
+<head>
+	<?php echo $this->html->charset(); ?>
+	<title>Unhandled exception</title>
+	<?php echo $this->html->style(array('debug', 'lithium')); ?>
+	<?php echo $this->scripts(); ?>
+	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+</head>
+<body class="app">
+	<div id="container">
+		<div id="header">
+			<h1>Some error!</h1>
+			<h3>Check the URL you have entered!</h3>
+		</div>
+		<div id="content">
+			<?php   echo $this->content(); ?>
+		</div>
+	</div>
+</body>
+</html>
+
 <!doctype html>
 <html>
 <head></head>
