428fbe296b0f7c077cc4113c214bd4d6908ae7a6
Nilam Doctor <nilamdoc@gmail.com>
2013-10-30 15:18:06 +0530

    emails

--- app/views/users/deposit.html.php
+++ app/views/users/deposit.html.php
@@ -26,22 +26,43 @@
 			<td><?=$data['Currency']?></td>
 		</tr>		
 </table>
+
 <p><strong><u><?=$t("Deposits/Withdrawals")?></u></strong></p>
 <blockquote>
 <ul>
 
 <li><?=$t("All deposits and withdrawals need to be verified and cleared, please see relevant sections when you login.")?></li>
+<li><?=$t("VERY IMPORTANT: Please make sure to INCLUDE your CUSTOMER REFERENCE with your deposit, which you can find when you complete FUNDING on your account page, so that we can credit your account appropriately.")?></li>
+<li><?=$t("We cannot be held liable if you send us money with no reference and have not completed a deposit request via your account (though with recorded delivery we can attempt to return any such fiat or solve such matters).")?></li>
+<li><?=$t("We cannot be held liable if you send us fiat with no reference, no deposit request, and no recorded delivery, and will treat such activity as suspicious and report it to the relevant authorities.")?></li>
+</ul>  
+<span><?=$t("When we receive your funds we verify with your deposit request and credit your IBWT account the amount.")?></span><br>
+<br>
+<p><strong><?=$t("Deposits")?></strong></p>
+<ul class="unstyled">
+<li><?=$t("You mail fiat via Royal Mail.")?></li>
+<li><?=$t("Fiat deposits are currently done via Royal Mail.")?>
+- <a href="http://www.royalmail.com/parcel-despatch-low" target="_blank"><?=$t("Parcel despatch.")?></a></li>
+<li><?=$t("Please make sure you read Royal Mails instructions for mailing fiat.")?> - <a href="http://www.royalmail.com/business/help-and-support/what-is-the-best-way-to-send-money-or-jewellery" target="_blank"><?=$t("Royal Mail - What is the best way to send money or jewellery.")?></a></li>
+<li><?=$t("We strongly recommend that you pay for Royal Mail compensations, relevent charges can be found here.")?> - <a href="http://www.royalmail.com/personal/uk-delivery/special-delivery"><?=$t("Royal Mail - Special delivery.")?></a></li>
 
-<li><strong><?=$t("IMPORTANT")?></strong>: <?=$t("Please make sure to wait for CLEARED deposit requests BEFORE depositing any sterling. As per our Terms Of Use, we cannot be held liable for any charges incurred due to deposits bounced if you have not had your deposit request CLEARED.")?></li>
-
-<li><strong><?=$t("VERY IMPORTANT")?></strong>: <?=$t("Please make sure to INCLUDE your CUSTOMER REFERENCE, which you can find on your account page, so that we can credit your account appropriately.")?></li>
-
-<u><?=$t("Example")?></u>: <?=$data['Reference']?></ul>
+<li><?=$t("Royal Mail offers extra services which you may or may not want to use, such as earlier delivery (by 9am (more expensive but faster) or 2nd class mail (cheaper but slower)).")?></li>
+<li><a href="http://www.royalmail.com/parcel-despatch-low/uk-delivery/special-delivery" target="_blank"><?=$t("Choose your options")?></a> or <a href="http://www.royalmail.com/parcel-despatch-low/uk-delivery/royal-mail-signed-2nd-class" target="_blank"><?=$t("2nd Class Mail")?></a></li>
+<li><?=$t("Royal Mail offers compensation cover of up to ")?>&pound;10,000. - <a href="http://www.royalmail.com/sites/default/files/RM%20Special%20Delivery%209am_Terms%20and%20Conditions_April%2012_0.pdf" target="_blank"><?=$t("Royal Mail Terms of Service.")?></a></li>
+<li><?=$t("Once fiat amounts are received your account gets credited the same amount, just the same as doing a bank transfer, without the bank.")?></li>
+</ul>
+<p><strong><?=$t("Withdrawals")?></strong></p>
+<ul class="unstyled">
+<li><?=$t("We charge customers the relevant fee that Royal Mail charges to cover the withdrawal amount respectively.")?></li>
+<li><?=$t("This charge is made to your IBWT account.")?></li>
+<li><?=$t("If you do not have enough to cover the Royal Mail fee in your IBWT account then your withdrawal will not be processed and you will be notified via email.")?></li>
+<li><?=$t("We store all fiat via safety deposit box services.")?></li>
+</ul>
 </blockquote>
 <p><strong><u><?=$t("Time Delays")?></u></strong></p>
 
 <blockquote>
-<u>GBP</u>
+<u>GBP / USD / EUR</u>
 <ul >
 <li><?=$t("Transfers are only processed weekdays, barring bank holidays.")?></li>
 <li><?=$t("It can take us up to 24 hours to verify and confirm your deposit request. Customers then have 24 hours to make their deposit. If a deposit is not made in the 24 window then IBWT assumes you have cancelled your deposit request and you will need to make request if you wish to deposit.")?></li>
@@ -49,6 +70,13 @@
 <li><?=$t("It can take us up to 24 hours to verify, confirm and start the process transfer for your withdrawal requests.")?></li>
 <li><?=$t("If the Customer's bank subscribes to Faster Payments the money will reach their account usually within 24 hours but can sometimes take up to close of business the next working day. Otherwise payments take 4 working days to process.")?></li>
 </ul>
+<u><?=$t("Bitcoin")?></u>
+<ul ><li><?=$t("Bitcoin deposits and withdrawals are subject to the Bitcoin protocol.")?></li></ul>
+<u><?=$t("Litecoin")?></u>
+<ul ><li><?=$t("Litecoin deposits and withdrawals are subject to the Litecoin protocol.")?></li></ul>
+</blockquote>
+
+
 <a href="/users/funding_btc" class="btn btn-primary">Funding BTC</a>
 <a href="/users/funding_ltc" class="btn btn-primary">Funding LTC</a>
 <a href="/users/funding_fiat" class="btn btn-primary">Funding Fiat</a>
--- app/views/users/deposit.mail.php
+++ app/views/users/deposit.mail.php
@@ -33,25 +33,48 @@
 <ul>
 
 <li>All deposits and withdrawals need to be verified and cleared, please see relevant sections when you login.</li>
+<li>VERY IMPORTANT: Please make sure to INCLUDE your CUSTOMER REFERENCE with your deposit, which you can find when you complete FUNDING on your account page, so that we can credit your account appropriately.</li>
+<li>We cannot be held liable if you send us money with no reference and have not completed a deposit request via your account (though with recorded delivery we can attempt to return any such fiat or solve such matters).</li>
+<li>We cannot be held liable if you send us fiat with no reference, no deposit request, and no recorded delivery, and will treat such activity as suspicious and report it to the relevant authorities.</li>
+</ul>  
+<span>When we receive your funds we verify with your deposit request and credit your IBWT account the amount.</span><br>
+<br>
+<p><strong>Deposits</strong></p>
+<ul class="unstyled">
+<li>You mail fiat via Royal Mail.</li>
+<li>Fiat deposits are currently done via Royal Mail.- <a href="http://www.royalmail.com/parcel-despatch-low" target="_blank">Parcel despatch.</a></li>
+<li>Please make sure you read Royal Mails instructions for mailing fiat. - <a href="http://www.royalmail.com/business/help-and-support/what-is-the-best-way-to-send-money-or-jewellery" target="_blank">Royal Mail - What is the best way to send money or jewellery.</a></li>
+<li>We strongly recommend that you pay for Royal Mail compensations, relevent charges can be found here. - <a href="http://www.royalmail.com/personal/uk-delivery/special-delivery">Royal Mail - Special delivery.</a></li>
 
-<li><strong>IMPORTANT</strong>: Please make sure to wait for CLEARED deposit requests BEFORE depositing any sterling. As per our Terms Of Use, we cannot be held liable for any charges incurred due to deposits bounced if you have not had your deposit request CLEARED.</li>
-
-<li><strong>VERY IMPORTANT</strong>: Please make sure to INCLUDE your CUSTOMER REFERENCE, which you can find on your account page, so that we can credit your account appropriately.</li>
-
-<u>Reference</u>: <?=$data['Reference']?></ul>
+<li>Royal Mail offers extra services which you may or may not want to use, such as earlier delivery (by 9am (more expensive but faster) or 2nd class mail (cheaper but slower)).</li>
+<li><a href="http://www.royalmail.com/parcel-despatch-low/uk-delivery/special-delivery" target="_blank">Choose your options</a> or <a href="http://www.royalmail.com/parcel-despatch-low/uk-delivery/royal-mail-signed-2nd-class" target="_blank">2nd Class Mail</a></li>
+<li>Royal Mail offers compensation cover of up to &pound;10,000. - <a href="http://www.royalmail.com/sites/default/files/RM%20Special%20Delivery%209am_Terms%20and%20Conditions_April%2012_0.pdf" target="_blank">Royal Mail Terms of Service.</a></li>
+<li>Once fiat amounts are received your account gets credited the same amount, just the same as doing a bank transfer, without the bank.</li>
+</ul>
+<p><strong>Withdrawals</strong></p>
+<ul class="unstyled">
+<li>We charge customers the relevant fee that Royal Mail charges to cover the withdrawal amount respectively.</li>
+<li>This charge is made to your IBWT account.</li>
+<li>If you do not have enough to cover the Royal Mail fee in your IBWT account then your withdrawal will not be processed and you will be notified via email.</li>
+<li>We store all fiat via safety deposit box services.</li>
+</ul>
 </blockquote>
 <p><strong><u>Time Delays</u></strong></p>
 
 <blockquote>
-<u>GBP</u>
+<u>GBP / USD / EUR</u>
 <ul >
 <li>Transfers are only processed weekdays, barring bank holidays.</li>
 <li>It can take us up to 24 hours to verify and confirm your deposit request. Customers then have 24 hours to make their deposit. If a deposit is not made in the 24 window then IBWT assumes you have cancelled your deposit request and you will need to make request if you wish to deposit.</li>
 <li>If your bank does not subscribe to Faster Payments (most UK banks do) then please let us know.</li>
 <li>It can take us up to 24 hours to verify, confirm and start the process transfer for your withdrawal requests.</li>
-<li>If the Customer's bank subscribes to Faster Payments the money will reach their account usually within 24 hours but can sometimes take up to close of business the next working day. Otherwise payments take 4 working days to process.</li>
+<li>If the Customer&#039;s bank subscribes to Faster Payments the money will reach their account usually within 24 hours but can sometimes take up to close of business the next working day. Otherwise payments take 4 working days to process.</li>
 </ul>
-
+<u>Bitcoin</u>
+<ul ><li>Bitcoin deposits and withdrawals are subject to the Bitcoin protocol.</li></ul>
+<u>Litecoin</u>
+<ul ><li>Litecoin deposits and withdrawals are subject to the Litecoin protocol.</li></ul>
+</blockquote>
 
 <p>Thanks,<br>
 <?=NOREPLY?></p>
