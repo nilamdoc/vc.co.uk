c90e79ac9282df200813d171da71c667242f2c43
Nilam Doctor <nilamdoc@gmail.com>
2013-11-20 13:50:57 +0530

    Funding-

--- app/views/sessions/add.html.php
+++ app/views/sessions/add.html.php
@@ -1,7 +1,7 @@
 <div style="background-image:url(/img/Stamp.png);background-position:bottom right;background-repeat:no-repeat">
 
 <p class="alert alert-error" style="font-size:16px;font-weight:bold;text-align:center ">
-Welcome to IBWT, alternative fundings methods now available, please see details in our recent <a href="/files/News Release 19-11-13.pdf" target="_blank">News Release</a>.</p>
+Welcome to IBWT, alternative funding methods now available, please see details in our recent <a href="/files/News Release 19-11-13.pdf" target="_blank">News Release</a>.</p>
 
 <div class="row">
 	<div class="span4 well" >
