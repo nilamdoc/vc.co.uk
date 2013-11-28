3e6187bdb4bd39cf3a33f2d8f5bd37562ee7ca94
Nilam Doctor <nilamdoc@gmail.com>
2013-11-28 15:48:53 +0530

    more

--- app/views/admin/play.html.php
+++ app/views/admin/play.html.php
@@ -13,9 +13,11 @@ function DisableUser(username){
 	$("#SecondUser option[value='"+username+"']").attr('disabled','disabled');
 	$("#SecondUser option[value='"+username+"']").attr('selected',false);	
 	$("#FirstUserForm").attr('action','/API/Trade/'+eval(username));
+	$("#FirstUserName").val(username);	
 }
 function SelectSecondUser(username){
 	$("#SecondUserForm").attr('action','/API/Trade/'+eval(username));
+	$("#SecondUserName").val(username);		
 }
 function TypeSelect(type){
 	if(type=="Sell"){
@@ -27,6 +29,10 @@ function TypeSelect(type){
 		$("#FirstUserType option[value='Buy']").attr("selected","selected");		
 		$("#SecondUserType option[value='Sell']").attr("selected","selected");				
 	}
+}
+function SelectPair(pair){
+	$("#FirstUserPair").val(pair);		
+	$("#SecondUserPair").val(pair);			
 }
 </script>
 <div class="row">
@@ -46,6 +52,17 @@ foreach($details as $detail){
 	<option value="Buy">Buy</option>
 	<option value="Sell">Sell</option>								
 	</select><br>
+	
+	Pair: <select name="Mainpair" class="span2" id="MainPair" onChange="SelectPair(this.value)">
+	<?php foreach($trades as $trade){
+		$FC = substr($trade['trade'],0,3);
+		$SC = substr($trade['trade'],4,3);
+	?>
+		<option value="<?=$FC?>_<?=$SC?>"><?=$FC?>_<?=$SC?></option>
+	<?php }?>
+	</select><br>
+	
+	
 	</form>
 </div>
 <div class="span5">
@@ -70,20 +87,14 @@ foreach($details as $detail){
 <div class="span5" style="border:1px solid black;padding:5px ">
 	<form action="/API/Trade/<?=$detail['key']?>" method="post" target="_blank" id="FirstUserForm" name="FirstUserForm">
 	<input type="hidden" name="nounce" value="<?=time()?>"><br>
+	Username: <input type="text" name="FirstUserName" id="FirstUserName" disabled="disabled"><br>
 	Type: <select name="type" class="span2" id="FirstUserType">
 	<option value="Buy">Buy</option>
 	<option value="Sell">Sell</option>								
 	</select><br>
-	Pair: <select name="pair" class="span2" id="FirstUserPair">
-	<?php foreach($trades as $trade){
-		$FC = substr($trade['trade'],0,3);
-		$SC = substr($trade['trade'],4,3);
-		$FCB = $detail['balance.'.$FC];
-		$SCB = $detail['balance.'.$SC];		
-	?>
-		<option value="<?=$FC?>_<?=$SC?>"><?=$FC?>(<?=$FCB?>)_<?=$SC?>(<?=$SCB?>)</option>
-	<?php }?>
-	</select><br>
+	Pair: <input type="text" name="pair" id="FirstUserPair" disabled="disabled"><br>
+
+
 	Amount: <input type="text" value="" placeholder="1.0" min="0.000001" max="9999" name="amount" id="FirstUseramountBTC" class="span2" value="0.1"><br>
 	Price: <input type="text" value="" placeholder="100.0" min="1" max="99999" name="price" id="FirstUserPerPriceBTC" class="span2" ><br>
 	</form>
@@ -91,20 +102,14 @@ foreach($details as $detail){
 <div class="span5" style="border:1px solid black;padding:5px ">
 	<form action="/API/Trade/<?=$detail['key']?>" method="post" target="_blank" id="FirstUserForm" name="SecondUserForm">
 	<input type="hidden" name="nounce" value="<?=time()?>"><br>
+	Username: <input type="text" name="SecondUserName" id="SecondUserName" disabled="disabled"><br>
 	Type: <select name="type" class="span2" id="SecondUserType">
 	<option value="Buy">Buy</option>
 	<option value="Sell">Sell</option>								
 	</select><br>
-	Pair: <select name="pair" class="span2" id="SecondUserPair">
-	<?php foreach($trades as $trade){
-		$FC = substr($trade['trade'],0,3);
-		$SC = substr($trade['trade'],4,3);
-		$FCB = $detail['balance.'.$FC];
-		$SCB = $detail['balance.'.$SC];		
-	?>
-		<option value="<?=$FC?>_<?=$SC?>"><?=$FC?>(<?=$FCB?>)_<?=$SC?>(<?=$SCB?>)</option>
-	<?php }?>
-	</select><br>
+	Pair: <input type="text" name="pair" id="SecondUserPair" disabled="disabled"><br>
+
+
 	Amount: <input type="text" value="" placeholder="1.0" min="0.000001" max="9999" name="amount" id="SecondUseramountBTC" class="span2" value="0.1"><br>
 	Price: <input type="text" value="" placeholder="100.0" min="1" max="99999" name="price" id="SecondUserPerPriceBTC" class="span2" ><br>
 	</form>
