77f5f9893627f9dfe358604d7cc45388a812c989
Nilam Doctor <nilamdoc@gmail.com>
2013-11-28 16:04:29 +0530

    final check

--- app/views/admin/play.html.php
+++ app/views/admin/play.html.php
@@ -34,85 +34,93 @@ function SelectPair(pair){
 	$("#FirstUserPair").val(pair);		
 	$("#SecondUserPair").val(pair);			
 }
+function ChangePrice(price){
+ $("#FirstUserPerPriceBTC").val(price);
+ $("#SecondUserPerPriceBTC").val(price); 
+}
+function ChangeAmount(amount){
+	$("#FirstUseramountBTC").val(amount);
+	$("#SecondUseramountBTC").val(amount);
+}
+function SubmitTrade(){
+	document.FirstUserForm.submit();
+	document.SecondUserForm.submit();		
+	return false;
+}
+
 </script>
 <div class="row">
-<div class="span5">
-	<form action="/API/Trade/<?=$detail['key']?>" method="post" target="_blank">
-	<input type="hidden" name="nounce" value="<?=time()?>"><br>
-	<select name="FirstUser" id="FirstUser" onChange="DisableUser(this.value);">
-	<option value="">--Select User --</option>
-<?php
-foreach($details as $detail){
-?>
-		<option value="<?=$detail['username']?>"><?=$detail['username']?></option>
-<?php }?>
-	</select><br>
-	Type: <select name="typeFirst" id="typeFirst" class="span2" onChange="TypeSelect(this.value)">
-	<option value="">--Select--</option>
-	<option value="Buy">Buy</option>
-	<option value="Sell">Sell</option>								
-	</select><br>
-	
-	Pair: <select name="Mainpair" class="span2" id="MainPair" onChange="SelectPair(this.value)">
-	<?php foreach($trades as $trade){
-		$FC = substr($trade['trade'],0,3);
-		$SC = substr($trade['trade'],4,3);
+	<div class="span5">
+		<form action="/API/Trade/SOMEKEY" method="post" target="_blank">
+		<input type="hidden" name="nounce" value="<?=time()?>"><br>
+		First User:
+		<select name="FirstUser" id="FirstUser" onChange="DisableUser(this.value);">
+		<option value="">--Select User --</option>
+	<?php
+	foreach($details as $detail){
 	?>
-		<option value="<?=$FC?>_<?=$SC?>"><?=$FC?>_<?=$SC?></option>
+			<option value="<?=$detail['username']?>"><?=$detail['username']?></option>
 	<?php }?>
-	</select><br>
-	
-	
-	</form>
-</div>
-<div class="span5">
-<form action="/API/Trade/<?=$detail['key']?>" method="post" target="_blank">
-	<input type="hidden" name="nounce" value="<?=time()?>"><br>
+		</select><br>
+		Second User:
 	<select name="SecondUser" id="SecondUser" onChange="SelectSecondUser(this.value)">
-	<option value="">--Select User --</option>	
-<?php
-foreach($details as $detail){
-?>
-<option value="<?=$detail['username']?>"><?=$detail['username']?></option>
-<?php }?>
-	</select><br>
-	Type: <select name="typeSecond" id="typeSecond" class="span2">
-	<option value="">--Select--</option>	
-	<option value="Buy">Buy</option>
-	<option value="Sell">Sell</option>								
-	</select><br>
-	
-	</form>
+		<option value="">--Select User --</option>	
+	<?php
+	foreach($details as $detail){
+	?>
+	<option value="<?=$detail['username']?>"><?=$detail['username']?></option>
+	<?php }?>
+		</select><br>
+		
+		First User Type: <select name="typeFirst" id="typeFirst" class="span2" onChange="TypeSelect(this.value)">
+		<option value="">--Select--</option>
+		<option value="Buy">Buy</option>
+		<option value="Sell">Sell</option>								
+		</select><br>
+		Second User Type: <select name="typeSecond" id="typeSecond" class="span2">
+		<option value="">--Select--</option>	
+		<option value="Buy">Buy</option>
+		<option value="Sell">Sell</option>								
+		</select><br>
+		
+		Pair: <select name="Mainpair" class="span2" id="MainPair" onChange="SelectPair(this.value)">
+		<option value="">--Select--</option>
+		<?php foreach($trades as $trade){
+			$FC = substr($trade['trade'],0,3);
+			$SC = substr($trade['trade'],4,3);
+		?>
+			<option value="<?=$FC?>_<?=$SC?>"><?=$FC?>_<?=$SC?></option>
+		<?php }?>
+		</select><br>
+		Amount: <input type="text" name="amount" id="UseramountBTC" class="span2" onBlur="ChangeAmount(this.value)"><br>
+		Price: <input type="text" name="price" id="UserPerPriceBTC" class="span2"  onBlur="ChangePrice(this.value)"><br>
+		<input type="submit" value="Submit Trade" onclick="SubmitTrade()">
+		</form>
+	</div>
 </div>
-<div class="span5" style="border:1px solid black;padding:5px ">
-	<form action="/API/Trade/<?=$detail['key']?>" method="post" target="_blank" id="FirstUserForm" name="FirstUserForm">
-	<input type="hidden" name="nounce" value="<?=time()?>"><br>
-	Username: <input type="text" name="FirstUserName" id="FirstUserName" disabled="disabled"><br>
-	Type: <select name="type" class="span2" id="FirstUserType">
-	<option value="Buy">Buy</option>
-	<option value="Sell">Sell</option>								
-	</select><br>
-	Pair: <input type="text" name="pair" id="FirstUserPair" disabled="disabled"><br>
-
-
-	Amount: <input type="text" value="" placeholder="1.0" min="0.000001" max="9999" name="amount" id="FirstUseramountBTC" class="span2" value="0.1"><br>
-	Price: <input type="text" value="" placeholder="100.0" min="1" max="99999" name="price" id="FirstUserPerPriceBTC" class="span2" ><br>
-	</form>
-</div>
-<div class="span5" style="border:1px solid black;padding:5px ">
-	<form action="/API/Trade/<?=$detail['key']?>" method="post" target="_blank" id="FirstUserForm" name="SecondUserForm">
-	<input type="hidden" name="nounce" value="<?=time()?>"><br>
-	Username: <input type="text" name="SecondUserName" id="SecondUserName" disabled="disabled"><br>
-	Type: <select name="type" class="span2" id="SecondUserType">
-	<option value="Buy">Buy</option>
-	<option value="Sell">Sell</option>								
-	</select><br>
-	Pair: <input type="text" name="pair" id="SecondUserPair" disabled="disabled"><br>
-
-
-	Amount: <input type="text" value="" placeholder="1.0" min="0.000001" max="9999" name="amount" id="SecondUseramountBTC" class="span2" value="0.1"><br>
-	Price: <input type="text" value="" placeholder="100.0" min="1" max="99999" name="price" id="SecondUserPerPriceBTC" class="span2" ><br>
-	</form>
-</div>
+<div class="row">
+	<div class="span5" style="border:1px solid black;padding:5px ">
+		<form action="/API/Trade/SOMEKEY" method="post" target="_blank" id="FirstUserForm" name="FirstUserForm">
+		<input type="hidden" name="nounce" value="<?=time()?>"><br>
+		Username: <input type="text" name="FirstUserName" id="FirstUserName" disabled="disabled"><br>
+		Type: <input name="type" type="text" class="span2" id="FirstUserType" disabled="disabled"><br>
+		Pair: <input type="text" name="pair" id="FirstUserPair" disabled="disabled"><br>
+		Amount: <input type="text" name="amount" id="FirstUseramountBTC" class="span2"  disabled="disabled"><br>
+		Price: <input type="text" name="price" id="FirstUserPerPriceBTC" class="span2"  disabled="disabled"><br>
+		</form>
+	</div>
+	<div class="span5" style="border:1px solid black;padding:5px ">
+		<form action="/API/Trade/SOMEKEY" method="post" target="_blank" id="FirstUserForm" name="SecondUserForm">
+		<input type="hidden" name="nounce" value="<?=time()?>"><br>
+		Username: <input type="text" name="SecondUserName" id="SecondUserName" disabled="disabled"><br>
+		Type: <select name="type" class="span2" id="SecondUserType">
+		<option value="Buy">Buy</option>
+		<option value="Sell">Sell</option>								
+		</select><br>
+		Pair: <input type="text" name="pair" id="SecondUserPair" disabled="disabled"><br>
+		Amount: <input type="text" name="amount" id="SecondUseramountBTC" class="span2"  disabled="disabled"><br>
+		Price: <input type="text" name="price" id="SecondUserPerPriceBTC" class="span2"  disabled="disabled"><br>
+		</form>
+	</div>
 
 </div>
