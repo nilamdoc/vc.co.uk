2755695c55144731161c2aa96de29aeb2005c065
Nilam Doctor <nilamdoc@gmail.com>
2014-01-06 17:23:32 +0530

    change

--- app/views/users/paymentbtcconfirm.html.php
+++ app/views/users/paymentbtcconfirm.html.php
@@ -6,7 +6,7 @@
 </script>
 <h4>Withdraw <?=abs($transaction['Amount'])?> BTC to <?=$transaction['address']?></h4>
 <form action="/users/payment/" method="post">
-<input type="hidden" name="username" id="username" value="<?=$transaction['username']?>"/>
+<input type="hidden" name="username" id="Username" value="<?=$transaction['username']?>"/>
 <input type="hidden" id="verify" value="<?=$transaction['verify.payment']?>" name="verify">
 <?=$this->form->field('password', array('type' => 'password', 'label'=>'Password', 'placeholder'=>'password','onfocus'=>'SendPassword();')); ?>
 		<div class="alert alert-danger"  style="display:none" id="LoginEmailPassword">
--- app/views/users/paymentltcconfirm.html.php
+++ app/views/users/paymentltcconfirm.html.php
@@ -3,7 +3,7 @@
 ?>
 <h4>Withdraw <?=abs($transaction['Amount'])?> BTC to <?=$transaction['address']?></h4>
 <form action="/users/paymentltc/" method="post">
-<input type="hidden" name="username" id="username" value="<?=$transaction['username']?>"/>
+<input type="hidden" name="username" id="Username" value="<?=$transaction['username']?>"/>
 <input type="hidden" id="verify" value="<?=$transaction['verify.payment']?>" name="verify">
 
 <?=$this->form->field('password', array('type' => 'password', 'label'=>'Password', 'placeholder'=>'password','onfocus'=>'SendPassword();')); ?>
