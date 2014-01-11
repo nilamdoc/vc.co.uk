443e7fe6f097c7882a55e0cce085d09a2542919e
Nilam Doctor <nilamdoc@gmail.com>
2014-01-11 07:51:06 +0530

    stuck button

--- app/views/users/paymentbtcconfirm.html.php
+++ app/views/users/paymentbtcconfirm.html.php
@@ -1,7 +1,7 @@
 <h4>Withdraw <?=abs($transaction['Amount'])?> BTC to <?=$transaction['address']?></h4>
-<form action="/users/payment/" method="post">
+<form action="/users/payment/" method="post" id="PaymentForm">
 <input type="hidden" name="username" id="Username" value="<?=$transaction['username']?>"/>
 <input type="hidden" id="verify" value="<?=$transaction['verify.payment']?>" name="verify">
 <?=$this->form->field('password', array('type' => 'password', 'label'=>'Password', 'placeholder'=>'password')); ?>
-<input type="submit" value="Confirm BTC Withdrawal" class="btn btn-success" id="PaymentBTCConfirm" onClick="$('#PaymentBTCConfirm').attr('disabled', 'disabled');return true;"> 
+<input type="submit" value="Confirm BTC Withdrawal" class="btn btn-success" id="PaymentBTCConfirm" onClick="document.getElementById('PaymentBTCConfirm').disabled = true;$('#PaymentForm').submit();"> 
 </form>
--- app/views/users/paymentltcconfirm.html.php
+++ app/views/users/paymentltcconfirm.html.php
@@ -1,7 +1,7 @@
 <h4>Withdraw <?=abs($transaction['Amount'])?> LTC to <?=$transaction['address']?></h4>
-<form action="/users/paymentltc/" method="post">
+<form action="/users/paymentltc/" method="post"  id="PaymentForm">
 <input type="hidden" name="username" id="Username" value="<?=$transaction['username']?>"/>
 <input type="hidden" id="verify" value="<?=$transaction['verify.payment']?>" name="verify">
 <?=$this->form->field('password', array('type' => 'password', 'label'=>'Password', 'placeholder'=>'password')); ?>
-<input type="submit" value="Confirm LTC Withdrawal" class="btn btn-success" id="PaymentLTCConfirm" onClick="$('#PaymentLTCConfirm').attr('disabled', 'disabled');return true;"> 
+<input type="submit" value="Confirm LTC Withdrawal" class="btn btn-success" id="PaymentLTCConfirm" onClick="document.getElementById('PaymentLTCConfirm').disabled = true;$('#PaymentForm').submit();"> 
 </form>
