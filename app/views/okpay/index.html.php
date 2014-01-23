8c9ac96314105f8537de7fa7037c18094bdd3d46
Nilam Doctor <nilamdoc@gmail.com>
2014-01-23 16:33:10 +0530

    ok

--- app/views/okpay/index.html.php
+++ app/views/okpay/index.html.php
@@ -1,17 +1,17 @@
 <div class="container " style="text-align:center;border:1px solid gray "><br>
-<p><strong>You can fund your OKPAY account via a bank wire transfer.</strong></p>
+<p  style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid gray "><strong>You can fund your OKPAY account via a bank wire transfer.</strong></p><br>
 <!-- Begin OKPAY Logo --><a HREF="https://www.okpay.com/?rbp=IBWT" target="_blank"><IMG SRC="https://www.okpay.com/img/partners/rbp_banner.gif" BORDER="0" ALT="Sign up for OKPAY and start accepting payments instantly."></a><!-- End OKPAY Logo --><br>
 <br>
-<p>Then you will be able to fund your IBWT account via OKPAY.</p>
+<p style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid gray ">Then you will be able to fund your IBWT account via OKPAY.</p><br>
 <img src="/img/Stamp.png" alt="IBWT"><br><br>
-<p>You will need submit standard information and get a verified account 
-with OKPAY to transfer funds via a wire transfer.</p>
+<div style="background-color:white;padding:2px;width:800px;margin:auto;border:1px solid gray ">
+	<p>You will need submit standard information and get a verified account with OKPAY to transfer funds via a wire transfer.</p>
+	<p>Once you have your funds in your OKPAY account you will be able to fund your IBWT account by sending funds to <a href="mailto:deposit@ibwt.co.uk">deposit@ibwt.co.uk</a>.</p><br>
+	<p  style="background-color:white;padding:2px;width:600px;margin:auto;border:1px solid red "><strong>Please make sure that you include your IBWT deposit reference in the comment.</strong></p><br>
+</div><br>
+	<img src="/img/OkPay-HowtoPay.png" alt="OKPAY - How to pay!"><br><br>
+	<p><strong>Once verified and cleared your funds will appear in your IBWT account.</strong></p>
 
-<p>Once you have your funds in your OKPAY account you will be able to fund your 
-IBWT account by sending funds to <a href="mailto:deposit@ibwt.co.uk">deposit@ibwt.co.uk</a>.</p><br>
-<img src="/img/OkPay-HowtoPay.png" alt="OKPAY - How to pay!"><br>
-<br>
-<p><strong>Once verified and cleared your funds will appear in your IBWT account.</strong></p>
 <!-- Begin OKPAY Seal Logo --><a href="https://www.okpay.com/account/user/456908980" target="_blank"><img src="https://www.okpay.com/img/seals/seal01.png" width="120"  height="120" border="0" alt="Verify OKPAY user information."></a><!-- End OKPAY Seal Logo --><br>
 <br>
 </div>
