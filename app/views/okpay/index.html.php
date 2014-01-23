22a5f8e91173a73b4a45154deab218dc7ab745ed
Nilam Doctor <nilamdoc@gmail.com>
2014-01-23 16:37:45 +0530

    done

--- app/views/okpay/index.html.php
+++ app/views/okpay/index.html.php
@@ -1,10 +1,10 @@
-<div class="container " style="text-align:center;border:1px solid gray "><br>
-<p  style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid gray "><strong>You can fund your OKPAY account via a bank wire transfer.</strong></p><br>
+<div class="container " style="text-align:center;border:1px solid #dddddd "><br>
+<p  style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid #dddddd "><strong>You can fund your OKPAY account via a bank wire transfer.</strong></p><br>
 <!-- Begin OKPAY Logo --><a HREF="https://www.okpay.com/?rbp=IBWT" target="_blank"><IMG SRC="https://www.okpay.com/img/partners/rbp_banner.gif" BORDER="0" ALT="Sign up for OKPAY and start accepting payments instantly."></a><!-- End OKPAY Logo --><br>
 <br>
-<p style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid gray ">Then you will be able to fund your IBWT account via OKPAY.</p><br>
+<p style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid #dddddd ">Then you will be able to fund your IBWT account via OKPAY.</p><br>
 <img src="/img/Stamp.png" alt="IBWT"><br><br>
-<div style="background-color:white;padding:2px;width:800px;margin:auto;border:1px solid gray ">
+<div style="background-color:white;padding:2px;width:800px;margin:auto;border:1px solid #dddddd ">
 	<p>You will need submit standard information and get a verified account with OKPAY to transfer funds via a wire transfer.</p>
 	<p>Once you have your funds in your OKPAY account you will be able to fund your IBWT account by sending funds to <a href="mailto:deposit@ibwt.co.uk">deposit@ibwt.co.uk</a>.</p><br>
 	<p  style="background-color:white;padding:2px;width:600px;margin:auto;border:1px solid red "><strong>Please make sure that you include your IBWT deposit reference in the comment.</strong></p><br>
