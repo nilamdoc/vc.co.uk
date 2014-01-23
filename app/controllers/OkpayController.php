4277f2bc5273488a510a468b09e1c38d028c5430
Nilam Doctor <nilamdoc@gmail.com>
2014-01-23 15:31:16 +0530

    back

---
+++ app/controllers/OkpayController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+use lithium\storage\Session;
+use app\models\Users;
+use app\models\Details;
+
+class OkpayController extends \lithium\action\Controller {
+
+	public function index(){
+
+	}
+}
+?>
