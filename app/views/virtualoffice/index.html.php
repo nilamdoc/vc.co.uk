daaa49e5ca8927d9f258b01453579541987e6775
Nilam Doctor <nilamdoc@gmail.com>
2014-01-25 16:02:17 +0530

    ok

--- app/views/virtualoffice/index.html.php
+++ app/views/virtualoffice/index.html.php
@@ -1,9 +1,13 @@
-<div style="background-image:url(/img/VO-Office.png);background-position:bottom right;background-repeat:no-repeat">
 <div class="row container" >
 	<div class="span11">
 		<div class="navbar">
 			<div class="navbar-inner1">
 			<a class="brand" href="#">Virtual Office</a>
+			<table background="/img/VO-Office.png" style="min-height:800px">
+				<tr>
+					<td></td>
+				</tr>
+			</table>
 			</div>
 		</div>
 	</div>
