84ad116651d1f14b80be0e46413c55eaba37e32e
Nilam Doctor <nilamdoc@gmail.com>
2014-01-25 16:10:23 +0530

    map

--- app/views/virtualoffice/index.html.php
+++ app/views/virtualoffice/index.html.php
@@ -4,8 +4,15 @@
 			<div class="navbar-inner1">
 				<a class="brand" href="#">Virtual Office</a>
 			</div>
-			<div style="min-height:800px;background-image:url(/img/VO-Office.png);background-position:bottom right;background-repeat:no-repeat">
-			aaa
+			<div style="min-height:800px;">
+			<img src="/img/VO-Office.png" border="0" usemap="#Map">
+      <map name="Map">
+        <area shape="rect" coords="413,61,485,147" href="#">
+        <area shape="rect" coords="503,59,575,150" href="#">
+        <area shape="rect" coords="909,71,975,165" href="#">
+        <area shape="rect" coords="399,285,503,375" href="#">
+        <area shape="rect" coords="661,277,748,355" href="#">
+      </map>
 			</div>			
 		</div>
 	</div>
