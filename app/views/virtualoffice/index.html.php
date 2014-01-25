8a86a6cdcf546e96eabc132800a3be3d1335862d
Nilam Doctor <nilamdoc@gmail.com>
2014-01-25 16:22:59 +0530

    title

--- app/views/virtualoffice/index.html.php
+++ app/views/virtualoffice/index.html.php
@@ -7,11 +7,11 @@
 			<div style="min-height:800px;">
 			<img src="/img/VO-Office.png" border="0" usemap="#Map">
       <map name="Map">
-        <area shape="rect" coords="413,61,485,147" href="/files/Certificate of Company Incorporation.pdf" alt="Certificate of Company Incorporation" target="_blank">
-        <area shape="rect" coords="503,59,575,150" href="/files/Certificate of Incorporation on Change of Name.pdf" alt="Certificate of Incorporation on Change of Name" target="_blank">
-        <area shape="rect" coords="909,71,975,165" href="/files/Certificate of Registration (ICO).pdf" alt="Certificate of Registration (ICO)" target="_blank">
-        <area shape="rect" coords="399,285,503,375" href="/print" alt="Print Paper Wallet">
-        <area shape="rect" coords="661,277,748,355" href="/dashboard" alt="Dashboard">
+        <area shape="rect" coords="413,61,485,147" href="/files/Certificate of Company Incorporation.pdf" alt="Certificate of Company Incorporation" title="Certificate of Company Incorporation" target="_blank">
+        <area shape="rect" coords="503,59,575,150" href="/files/Certificate of Incorporation on Change of Name.pdf" alt="Certificate of Incorporation on Change of Name" title="Certificate of Incorporation on Change of Name" target="_blank">
+        <area shape="rect" coords="909,71,975,165" href="/files/Certificate of Registration (ICO).pdf" alt="Certificate of Registration (ICO)" title="Certificate of Registration (ICO)" target="_blank">
+        <area shape="rect" coords="399,285,503,375" href="/print" alt="Print Paper Wallet"  title="Print Paper Wallet">
+        <area shape="rect" coords="661,277,748,355" href="/dashboard" alt="Dashboard" title="Dashboard">
       </map>
 			</div>			
 		</div>
