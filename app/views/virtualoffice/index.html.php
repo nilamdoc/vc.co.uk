e82772e80b57151defe9d3acaa32f8969c3d4cec
Nilam Doctor <nilamdoc@gmail.com>
2014-01-26 12:46:49 +0530

    vo

--- app/views/virtualoffice/index.html.php
+++ app/views/virtualoffice/index.html.php
@@ -4,15 +4,66 @@
 			<div class="navbar-inner1">
 				<a class="brand" href="#">Virtual Office</a>
 			</div>
-			<div style="min-height:800px;">
-			<img src="/img/VO-Office.png" border="0" usemap="#Map">
-      <map name="Map">
-        <area shape="rect" coords="413,61,485,147" href="/files/Certificate of Company Incorporation.pdf" alt="Certificate of Company Incorporation" title="Certificate of Company Incorporation" target="_blank"  class="tooltip-x" rel="tooltip-x" data-placement="top">
-        <area shape="rect" coords="503,59,575,150" href="/files/Certificate of Incorporation on Change of Name.pdf" alt="Certificate of Incorporation on Change of Name" title="Certificate of Incorporation on Change of Name" target="_blank"  class="tooltip-x" rel="tooltip-x" data-placement="top">
-        <area shape="rect" coords="909,71,975,165" href="/files/Certificate of Registration (ICO).pdf" alt="Certificate of Registration (ICO)" title="Certificate of Registration (ICO)" target="_blank"  class="tooltip-x" rel="tooltip-x" data-placement="top">
-        <area shape="rect" coords="399,285,503,375" href="/print" alt="Print Paper Wallet"  title="Print Paper Wallet" class="tooltip-x" rel="tooltip-x" data-placement="top">
-        <area shape="rect" coords="661,277,748,355" href="/ex/dashboard" alt="Dashboard" title="Dashboard" class="tooltip-x" rel="tooltip-x" data-placement="top">
-      </map>
+			<div style="min-height:825px;">
+				<table border="0" cellpadding="0" cellspacing="0" width="1100">
+					<tr>
+					 <td><img src="/img/VO/spacer.gif" width="405" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="89" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="84" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="75" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="102" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="150" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="84" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="110" height="1" border="0" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="1" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td colspan="9"><img name="VirtualOffice_r1_c1" src="/img/VO/VirtualOffice_r1_c1.jpg" width="1100" height="62" border="0" id="VirtualOffice_r1_c1" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="62" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="5" colspan="2"><img name="VirtualOffice_r2_c1" src="/img/VO/VirtualOffice_r2_c1.jpg" width="406" height="224" border="0" id="VirtualOffice_r2_c1" alt="" /></td>
+					 <td rowspan="2"><a href=""><img name="VirtualOffice_r2_c3" src="/img/VO/VirtualOffice_r2_c3.jpg" width="89" height="103" border="0" id="VirtualOffice_r2_c3" alt="Certificate" title="Certificate" /></a></td>
+					 <td><img name="VirtualOffice_r2_c4" src="/img/VO/VirtualOffice_r2_c4.jpg" width="84" height="102" border="0" id="VirtualOffice_r2_c4" alt="" /></td>
+					 <td rowspan="4" colspan="3"><img name="VirtualOffice_r2_c5" src="/img/VO/VirtualOffice_r2_c5.jpg" width="327" height="222" border="0" id="VirtualOffice_r2_c5" alt="" /></td>
+					 <td rowspan="3"><img name="VirtualOffice_r2_c8" src="/img/VO/VirtualOffice_r2_c8.jpg" width="84" height="111" border="0" id="VirtualOffice_r2_c8" alt="" /></td>
+					 <td rowspan="8"><img name="VirtualOffice_r2_c9" src="/img/VO/VirtualOffice_r2_c9.jpg" width="110" height="763" border="0" id="VirtualOffice_r2_c9" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="102" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="7"><img name="VirtualOffice_r3_c4" src="/img/VO/VirtualOffice_r3_c4.jpg" width="84" height="661" border="0" id="VirtualOffice_r3_c4" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="1" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="3"><img name="VirtualOffice_r4_c3" src="/img/VO/VirtualOffice_r4_c3.jpg" width="89" height="121" border="0" id="VirtualOffice_r4_c3" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="8" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="5"><img name="VirtualOffice_r5_c8" src="/img/VO/VirtualOffice_r5_c8.jpg" width="84" height="652" border="0" id="VirtualOffice_r5_c8" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="111" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="4"><img name="VirtualOffice_r6_c5" src="/img/VO/VirtualOffice_r6_c5.jpg" width="75" height="541" border="0" id="VirtualOffice_r6_c5" alt="" /></td>
+					 <td rowspan="2"><img name="VirtualOffice_r6_c6" src="/img/VO/VirtualOffice_r6_c6.jpg" width="102" height="71" border="0" id="VirtualOffice_r6_c6" alt="" /></td>
+					 <td rowspan="4"><img name="VirtualOffice_r6_c7" src="/img/VO/VirtualOffice_r6_c7.jpg" width="150" height="541" border="0" id="VirtualOffice_r6_c7" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="2" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="3"><img name="VirtualOffice_r7_c1" src="/img/VO/VirtualOffice_r7_c1.jpg" width="405" height="539" border="0" id="VirtualOffice_r7_c1" alt="" /></td>
+					 <td rowspan="2" colspan="2"><img name="VirtualOffice_r7_c2" src="/img/VO/VirtualOffice_r7_c2.jpg" width="90" height="85" border="0" id="VirtualOffice_r7_c2" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="69" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td rowspan="2"><img name="VirtualOffice_r8_c6" src="/img/VO/VirtualOffice_r8_c6.jpg" width="102" height="470" border="0" id="VirtualOffice_r8_c6" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="16" border="0" alt="" /></td>
+					</tr>
+					<tr>
+					 <td colspan="2"><img name="VirtualOffice_r9_c2" src="/img/VO/VirtualOffice_r9_c2.jpg" width="90" height="454" border="0" id="VirtualOffice_r9_c2" alt="" /></td>
+					 <td><img src="/img/VO/spacer.gif" width="1" height="454" border="0" alt="" /></td>
+					</tr>
+				</table>
+			
 			</div>			
 		</div>
 	</div>
