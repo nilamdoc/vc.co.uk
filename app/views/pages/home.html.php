20c5bc45846b1a9d566c3ab356ccd20bbea1fda8
Nilam Doctor <nilamdoc@gmail.com>
2014-01-27 12:26:47 +0530

    home

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -58,16 +58,18 @@ if(substr(Environment::get('locale'),0,2)=="en"){$locale = "en";}else{$locale = 
 	<div class="span3 bs-docs-site" style="min-height:732px; ">
 		<div style="overflow:auto;height:605px">
 			<ul class="unstyled">
-				<li class="alert alert-danger"><a href="/<?=$locale?>/news"><strong>News</strong></a></li>
-				<li class="alert alert-danger"><a href="https://www.youtube.com/watch?v=LP4GSvQUtBw" target="_blank"><strong>Explanation: Bitcoin - The Future Currency</strong></a></li>
+				<li class="alert alert-danger"><strong>Visit our <a href="/<?=$locale?>/virtualoffice">Virtual Office</a> for the latest IBWT <a href="/<?=$locale?>/news">News</a> and Info.</strong></li>
+			<div style="text-align:center ">
+					<a href="/<?=$locale?>/virtualoffice"><img src="/img/VO-Office.jpg" width="225px"  style="margin-bottom:10px;margin-top:-10px"></a>
+			</div>				
+				<li class="alert alert-danger"><a href="https://www.youtube.com/watch?v=LP4GSvQUtBw" target="_blank"><strong>Bitcoin - The Future Currency</strong></a></li>
 			</ul>
 		<div style="text-align:center ">
-
-<!-- Begin OKPAY Seal Logo --><a href="https://www.okpay.com/account/user/456908980" target="_blank"><img src="https://www.okpay.com/img/seals/seal01.png" width="120"  height="120" border="0" alt="Verify OKPAY user information."></A><!-- End OKPAY Seal Logo --><br><br>
+<!-- Begin OKPAY Seal Logo --><a href="https://www.okpay.com/account/user/456908980" target="_blank"><img src="https://www.okpay.com/img/seals/seal01.png" width="100"  height="100" border="0" style="margin-bottom:10px" alt="Verify OKPAY user information."></A><!-- End OKPAY Seal Logo --><br>
 <!--		<a href="http://www.bitcointrezor.com/" target="_blank"><img src="/img/trezor.png" title="Trezor, the Bitcoin Safe"></a><br> -->
-		<a href="http://bitcoinmagazine.com/" target="_blank"><img src="/img/BMlogo.jpg" title="Bitcoin Magazine"></a><br><br>
+		<a href="http://bitcoinmagazine.com/" target="_blank"><img src="/img/BMlogo.jpg" title="Bitcoin Magazine"  style="margin-bottom:10px" width="150"></a><br>
 
-		<a href="https://bitcoinfoundation.org/" target="_blank"><img src="/img/bitcoin-foundation.jpg" alt="Bitcoin Foundation" width="200px"></a><br>
+		<a href="https://bitcoinfoundation.org/" target="_blank"><img src="/img/bitcoin-foundation.jpg" alt="Bitcoin Foundation" width="170px"></a><br>
 		</div>
 		</div>
 
