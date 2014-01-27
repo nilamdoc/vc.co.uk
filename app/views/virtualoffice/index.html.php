fb98bec35e78ba769ab995d6928802434c88cef4
Nilam Doctor <nilamdoc@gmail.com>
2014-01-27 12:11:57 +0530

    do

--- app/views/virtualoffice/index.html.php
+++ app/views/virtualoffice/index.html.php
@@ -1,10 +1,10 @@
 <div class="row container" >
-	<div class="span11">
+	<div class="span12">
 		<div class="navbar">
 			<div class="navbar-inner1">
 				<a class="brand" href="#">Virtual Office</a>
 			</div>
-			<iframe frameborder="1" src="/VCOffice.html" height="885px" width="1160px"></iframe>
+			<iframe frameborder="1" src="/VCOffice.html" height="855px" width="1130px"></iframe>
 		</div>
 	</div>
 </div>
