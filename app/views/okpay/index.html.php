b7fd513c68bd39f269b8f4a319b8011a1e909ac2
Nilam Doctor <nilamdoc@gmail.com>
2014-02-26 17:24:40 +0530

    ok

--- app/views/okpay/index.html.php
+++ app/views/okpay/index.html.php
@@ -2,6 +2,20 @@
 <p  style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid #dddddd "><strong>You can fund your OKPAY account via a bank wire transfer.</strong></p><br>
 <!-- Begin OKPAY Logo --><a HREF="https://www.okpay.com/?rbp=IBWT" target="_blank"><IMG SRC="https://www.okpay.com/img/partners/rbp_banner.gif" BORDER="0" ALT="Sign up for OKPAY and start accepting payments instantly."></a><!-- End OKPAY Logo --><br>
 <br>
+<div style="background-color:white;padding:6px;width:800px;margin:auto;border:1px solid #dddddd;text-align:left;">
+
+<p>OKPAY is a P2P and B2C web based payment system providing users with a virtual account that allows them to easily load, send, receive, and withdraw money as well as make purchases online. OKPAY is a widely spread electronic payment system and is among the world's leading eWallet providers; it offers access to a great number of payment options for businesses in over 200 countries and territories within a single integration.</p>
+<p>OKPAY is a fully-fledged payment service provider suitable for any type of online business. Its main features are:
+
+<ul style="text-align:left; ">
+    <li>Instant and secure money transfers. All you need to know is an e-mail address or an OKPAY Wallet ID.</li>
+    <li>Make purchases and receive payments without disclosing personal and financial information.</li>
+    <li>Wide range of deposit and withdrawal options: Bank Transfers or Checks, Various Electronic Currencies, Money Transfers, Phone Payments, etc.</li>
+    <li>OKPAY Card - is one of the most convenient ways to get money out of your OKPAY account. Use it worldwide at ATMs, online, and in local stores anywhere MasterCard is accepted.</li>
+    <li>Advanced account security system based on one-time PIN codes sent via SMS to a client's mobile phone.</li>
+</ul>		
+</p>		
+</div>
 <p style="background-color:white;padding:2px;width:500px;margin:auto;border:1px solid #dddddd ">Then you will be able to fund your IBWT account via OKPAY.</p><br>
 <img src="/img/Stamp.png" alt="IBWT"><br><br>
 <div style="background-color:white;padding:2px;width:800px;margin:auto;border:1px solid #dddddd ">
