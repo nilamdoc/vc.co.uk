439f003c914ee30e15228abf64b131e908bd62c0
Nilam Doctor <nilamdoc@gmail.com>
2014-03-03 11:45:26 +0530

    save

--- app/views/admin/hard.html.php
+++ app/views/admin/hard.html.php
@@ -1,6 +1,9 @@
 <form name="Admin_Hard" method="post" action="/Admin/hard" class="form-horizontal">
-		<input type="text" name="withdrawal.GBP" id="withdrawal.GBP" placeholder="0" value="<?=$Withdrawal['withdrawal']['GBP']?>" class="span2">
-		<input type="text" name="withdrawal.USD" id="withdrawal.USD" placeholder="0" value="<?=$Withdrawal['withdrawal']['USD']?>" class="span2">
-		<input type="text" name="withdrawal.EUR" id="withdrawal.EUR" placeholder="0" value="<?=$Withdrawal['withdrawal']['EUR']?>" class="span2">		
+GBP:		<input type="text" name="withdrawal[GBP]" id="withdrawalGBP" placeholder="0" value="<?=$Withdrawal['withdrawal']['GBP']?>" class="span2"><br>
+
+USD:		<input type="text" name="withdrawal[USD]" id="withdrawalUSD" placeholder="0" value="<?=$Withdrawal['withdrawal']['USD']?>" class="span2"><br>
+
+EUR:		<input type="text" name="withdrawal[EUR]" id="withdrawalEUR" placeholder="0" value="<?=$Withdrawal['withdrawal']['EUR']?>" class="span2">		<br>
+
 		<input type="submit" value="Save" class="btn btn-primary ">
 </form>
