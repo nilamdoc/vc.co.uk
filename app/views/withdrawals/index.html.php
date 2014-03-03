4f43536a95abb57ce0bfa3d7a609e572da057c68
Nilam Doctor <nilamdoc@gmail.com>
2014-03-03 11:32:39 +0530

    padding

--- app/views/withdrawals/index.html.php
+++ app/views/withdrawals/index.html.php
@@ -1,4 +1,4 @@
-<div class="container " style="text-align:center;border:1px solid #dddddd "><br>
+<div class="container " style="text-align:center;border:1px solid #dddddd;padding:10px "><br>
 <p>Due to the probabilities of customers depositing fiat to their IBWT accounts via OKPAY and then customers withdrawing their GBP via Hard Currency, direct into banks or via Royal Mail. IBWT operates a '<strong>Liquidity Measure</strong>'.</p>
 <p>Below can be seen the current amount that can be withdrawn via hard currency. If you wish to withdraw more fiat that is currently available via hard currency, then we suggest that you use our OKPAY withdrawal system (you will need a bank account and OKPAY verification for this).</p>
 <p><a href="" class="btn btn-primary"><?=number_format($Withdrawal['withdrawal']['GBP'],2)?> GBP</a></p>
