416d6b1d32c82df832b1543e52d0c497652f9026
Nilam Doctor <nilamdoc@gmail.com>
2014-03-03 11:27:19 +0530

    parameters

--- app/controllers/WithdrawalsController.php
+++ app/controllers/WithdrawalsController.php
@@ -1,11 +1,15 @@
 <?php
 namespace app\controllers;
+use lithium\storage\Session;
+use app\models\Parameters;
 
 class WithdrawalsController extends \lithium\action\Controller {
 	public function index(){
 		$user = Session::read('default');
-		if ($user==""){		return $this->redirect('/login');}
+//		if ($user==""){		return $this->redirect('/login');}
 		$id = $user['_id'];
+		$Withdrawal = Parameters::find('first');
+		
 		return compact('Withdrawal');
 	}
 
