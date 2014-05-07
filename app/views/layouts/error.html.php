9f3f8cbb6737bf80ba76d1d7a44baf91ce62989d
Nilam Doctor <nilamdoc@gmail.com>
2014-05-07 11:11:40 +0530

    err

--- app/views/layouts/error.html.php
+++ app/views/layouts/error.html.php
@@ -1,10 +1,274 @@
 <!doctype html>
 <html>
-<head></head>
-<body>
-<h4>In Bitcoin We Trust</h4>
-<p>Please report the steps you did to to get to this page to support@ibwt.co.uk</p>
-<a href="https://ibwt.co.uk">Go back to site!</a>
-<?php //echo $this->content(); ?>
+<head>
+	<meta charset="UTF-8" />	<title>IBWT | Digital Currency Exchange - Buy & Sell Bitcoin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<meta name="keywords" content="ibwt, ibwt homepage, bitcoin, buy bitcoin, sell bitcoin, litecoin, cryptocurrency, exchange, gbp, UK, btc, bitcoin exchange, digital currency exchange, greencoin">	
+	<meta name="description" content="A digital currency exchange where you can buy or sell Bitcoin and other Cryptocurrencies with ease via multiple funding methods to suit your needs.">		
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/datepicker.css" />
+	
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap-responsive.css" />
+	
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/docs.css" />
+	
+	<link href="/favicon.ico" title="Icon" type="image/x-icon" rel="icon" />
+	<link href="/favicon.ico" title="Icon" type="image/x-icon" rel="shortcut icon" />
+	<script src="/bootstrap/js/jquery.js"></script>	
+	<script src="/bootstrap/js/bootstrap-datepicker.js"></script>	
+	   		
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 20px;
+      }
+      /* Custom container */
+      .container {
+        margin: 0 auto; 
+      }
+      .container > hr {
+        margin: 20px 0;
+      }
+    </style>
+</head>
+<body  onLoad="CheckServer();"  style="background-image:url(/img/worldmap.png);background-position:top;background-repeat:no-repeat ">
+	<div id="container" class="container-fluid">
+		<div class="navbar navbar-fixed-top" >
+	<div class="navbar-inner" style="width: auto; padding: 0 20px;">
+		<div class="container"  style="width: 90%; padding: 0 20px;" >
+			<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</a>
+			<!-- Be sure to leave the brand out there if you want it shown -->
+			<a class="brand" href="/en/"><img src="/img/ibwt.co.uk.gif" alt="In Bitcoin We Trust" title="In Bitcoin We Trust"></a>
+			<!-- Everything you want hidden at 940px or less, place within here -->
+		<div>
+
+			<!-- .nav, .navbar-search, .navbar-form, etc -->
+				<div class="nav-collapse collapse">
+								<ul class="nav">				
+					<li><a  style="font-size:30px;color:#333;font-weight:bold;" >Virtual Currency Exchange</a></li>
+				</ul>
+								
+				</div>
+			</div>
+			<ul class="nav pull-right">
+												<a href="/en/login" class="btn">Login / Register</a>			
+							
+			</ul>			
+		</div>
+	</div>
+</div>
+<img src="/img/half-ibwt.co.uk.png" class="pull-right" style="margin-top:7px">
+				<h4>Dashboard</h4>
+<ul class="nav nav-tabs push-right">
+		<li class='active'>
+		<a href="/en/ex/dashboard/" style="cursor:pointer "><img src="/img/dashboard.png" alt="Dashboard"></a>
+	</li>
+					<li >
+			<a href="/en/ex/x/btc_gbp" class="tooltip-x" rel="tooltip-x" data-placement="top" title="BTC/GBP">
+			<img src="/img/Bitcoin.png">&raquo;<img src="/img/Pound.png">
+			</a></li>
+					<li >
+			<a href="/en/ex/x/btc_eur" class="tooltip-x" rel="tooltip-x" data-placement="top" title="BTC/EUR">
+			<img src="/img/Bitcoin.png">&raquo;<img src="/img/Euro.png">
+			</a></li>
+					<li >
+			<a href="/en/ex/x/btc_usd" class="tooltip-x" rel="tooltip-x" data-placement="top" title="BTC/USD">
+			<img src="/img/Bitcoin.png">&raquo;<img src="/img/Dollar.png">
+			</a></li>
+					<li >
+			<a href="/en/ex/x/btc_ltc" class="tooltip-x" rel="tooltip-x" data-placement="top" title="BTC/LTC">
+			<img src="/img/Bitcoin.png">&raquo;<img src="/img/Litecoin.png">
+			</a></li>
+					<li >
+			<a href="/en/ex/x/ltc_gbp" class="tooltip-x" rel="tooltip-x" data-placement="top" title="LTC/GBP">
+			<img src="/img/Litecoin.png">&raquo;<img src="/img/Pound.png">
+			</a></li>
+				<li  class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">More... <b class="caret"></b></a>
+		    <ul class="dropdown-menu">
+																																	<li >
+			<a href="/en/ex/x/ltc_usd" class="tooltip-x" rel="tooltip-x" data-placement="top" title="LTC/USD">
+			<img src="/img/Litecoin.png">&raquo;<img src="/img/Dollar.png">
+			</a></li>
+											<li >
+			<a href="/en/ex/x/ltc_eur" class="tooltip-x" rel="tooltip-x" data-placement="top" title="LTC/EUR">
+			<img src="/img/Litecoin.png">&raquo;<img src="/img/Euro.png">
+			</a></li>
+											<li >
+			<a href="/en/ex/x/btc_cad" class="tooltip-x" rel="tooltip-x" data-placement="top" title="BTC/CAD">
+			<img src="/img/Bitcoin.png">&raquo;<img src="/img/CDollar.png">
+			</a></li>
+											<li >
+			<a href="/en/ex/x/ltc_cad" class="tooltip-x" rel="tooltip-x" data-placement="top" title="LTC/CAD">
+			<img src="/img/Litecoin.png">&raquo;<img src="/img/CDollar.png">
+			</a></li>
+											<li >
+			<a href="/en/ex/x/btc_xgc" class="tooltip-x" rel="tooltip-x" data-placement="top" title="BTC/XGC">
+			<img src="/img/Bitcoin.png">&raquo;<img src="/img/Greencoin.png">
+			</a></li>
+									
+				</ul>
+		</li>
+		
+</ul>
+			
+		<div class="row container">
+	<div class="well span8" >
+	<table>
+		<tr>
+			<td><img src="/img/IBWTLogo.png" width="240" alt="In Bitcoin We Trust"></td>
+			<td><!-- Begin OKPAY Logo --><A HREF="https://www.okpay.com/?rbp=IBWT" target="_blank"><IMG SRC="https://www.okpay.com/img/partners/rbp_banner.gif" BORDER="0" ALT="Sign up for OKPAY and start accepting payments instantly."></A><!-- End OKPAY Logo --></td>
+		</tr>
+	</table>
+	<blockquote>"The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous 
+	centuries of its existence." <small>Nikola Tesla</small></blockquote>
+	<p class="alert" style="color:#111111;background-color:#FCC952 ;font-size:16px;font-weight:bold ">Buy and Sell Bitcoins at a low 0.3% Commission Fee. Use our free <a href="/print">Paper Wallet</a> cold storage service, your best suited <a href="/company/funding">Alternative Funding</a> system, and use Bank Wire Transfers via <a href="/okpay">OKPAY</a>.</p>
+	<h5>IBWT is a UK Bitcoin/Virtual Currency exchange, offering a fully regulated, secure method, for individuals and businesses to buy or sell bitcoins.</h5>
+	<ul>
+			<li>Fees are <strong>0.3</strong>% per transaction</li>
+			<li>Simple verification means you could be a full customer in a matter of days</li>
+			<li>Security ensured with Cold Storage, SSL 256bit encryption & 2FA</li>
+			<li>Dedicated Server for an enhanced customer experience</li>
+			<li>Deposits via <A HREF="/okpay" target="_blank">OKPAY</A> or secure mail services</li>
+			<li>Withdrawal via <A HREF="/okpay" target="_blank">OKPAY</A>, secure mail services or via banks</li>
+
+	</ul>
+	<h3>About Bitcoin</h3>
+	<ul>
+		<li><a href="http://www.coindesk.com/information/" target="_blank">Beginners Guide To Bitcoin</a></li>
+		<li><a href="http://bitcoin.org/en/" target="_blank">Bitcoin Organization</a></li>
+		<li><a href="https://en.bitcoin.it/wiki/Main_Page" target="_blank">Bitcoin Wiki</a></li>
+		<li><a href="http://bitcoinmagazine.com/" target="_blank">Bitcoin Magazine</a></li>
+	</ul>
+	<h3>Security</h3>
+	<ul>
+		<li>We use <strong>Two Factor Authentication</strong> for your account to login to ibwt.co.uk.</li>
+		<li>We use <strong>Time-based One-time Password Algorithm (TOTP)</strong> for login, withdrawal/deposits and settings.</li>
+		<li>Keep your Bitcoins safe and secure - <a href="http://bitcoin.org/en/secure-your-wallet" target="_blank">Security</a>.</li>
+	</ul>
+<h3>Find us</h3>
+<ul class="unstyled">
+	<li>
+		<a  target="_blank" href="https://www.facebook.com/pages/IBWT/425446187570505"><img src="/img/Facebook-logo.png" alt="Facebook" width="30px" title="Facebook"></a>
+		&nbsp;&nbsp;
+		<a target="_blank" href="https://twitter.com/IBWTofficial"><img src="/img/twitter.jpg" alt="Twitter" width="30px" title="Twitter"></a>
+		&nbsp;&nbsp;
+		<a target="_blank" href="http://www.reddit.com/r/IBWTofficial/"><img src="/img/reddit.jpg" alt="Reddit" width="30px" title="Reddit"></a>
+		&nbsp;&nbsp;
+<a href="https://bitcointalk.org/index.php?topic=397625.0" target="_blank"><img src="/img/bitcointalk_logo.jpg.png" alt="Bitcoin talk" width="30px" title="bitcoin talk"></a>
+		&nbsp;&nbsp;
+		<a href="https://plus.google.com/b/100582829535245250566/100582829535245250566/about?hl=en" target="_blank"><img src="/img/google.png" width="30px" alt="Google+" title="Google+"></a>
+		&nbsp;&nbsp;
+		<a href="https://www.linkedin.com/company/in-bitcoin-we-trust-jd-ltd" target="_blank"><img src="/img/linkedin.jpg" width="30" alt="LinkedIn" title="Linkedin"></a>
+				&nbsp;&nbsp;
+<a target="_blank" href="https://plus.google.com/+IbwtCoUk" rel="publisher" /><img src="/img/google.png" width="30px" alt="Google+" title="Google+"></a>
+	</li>
+	
+</ul>
+
+	</div>
+	<div class="span3" style="min-height:792px;border:1px solid gold;padding:4px;text-align:center ">
+		<div style="overflow:auto;height:655px">
+			<ul class="unstyled">
+				<li class="alert alert-danger" style="text-align:center "><strong>Visit our <a href="/en/virtualoffice">Virtual Office</a> for the latest IBWT <a href="/en/news">News</a> and Info.</strong></li>
+			<div style="text-align:center ">
+					<a href="/en/virtualoffice"><img src="/img/VO-Office.jpg" width="225px"  style="margin-bottom:10px;margin-top:-10px"></a>
+			</div>				
+				<li class="alert" style="background-color:#FCC952;text-align:center"><a href="https://www.youtube.com/watch?v=LP4GSvQUtBw" target="_blank"><strong>Bitcoin - The Future Currency</strong></a></li>
+			</ul>
+		<div style="text-align:center ">
+<!-- Begin OKPAY Seal Logo --><a href="https://www.okpay.com/account/user/456908980" target="_blank"><img src="https://www.okpay.com/img/seals/seal01.png" width="100"  height="100" border="0" style="margin-bottom:10px" alt="Verify OKPAY user information."></a>
+	<a href="/okpay"><img src="/img/okpay-excp-b.png" width="100"  height="100" border="0" style="margin-bottom:10px" alt="Verify OKPAY user information.">
+</a><!-- End OKPAY Seal Logo --><br>
+<!--		<a href="http://www.bitcointrezor.com/" target="_blank"><img src="/img/trezor.png" title="Trezor, the Bitcoin Safe"></a><br> -->
+		<a href="http://bitcoinmagazine.com/" target="_blank"><img src="/img/BMlogo.jpg" title="Bitcoin Magazine"  style="margin-bottom:10px" width="150"></a><br>
+<a href="https://multibit.org/" target="_blank"><img src="/img/multibit-banner.png" width="170px" alt="Multibit"/></a><br><br>
+<a href="http://bitgivefoundation.org/" target="_blank"><img src="/img/bitgivetile.png" width="170px" alt="BitGive"></a><br>
+<!--		<a href="https://bitcoinfoundation.org/" target="_blank"><img src="/img/bitcoin-foundation.jpg" alt="Bitcoin Foundation" width="170px"></a><br>
+-->
+
+		</div>
+		</div>
+
+		<img src="/img/Stamp.png"  width="220" style="padding:1px;">
+	</div>
+<!--
+<h3>News</h3>
+<ul>
+	<li><a href="/files/News Release 27-09-13.pdf">Launching on 30th September 2013</a></li>
+	<li>Please find our recent news <a href="/news">here.</a>.</li>
+</ul>
+-->
+
+<br>
+<br>
+<br>
+<br>
+</div>	</div>
+	<div id="footer" style="padding:1px 20px; margin-top:20px;border-top:1px solid gray;" class="navbar-inner navbar ">
+	<ul class="nav" style="font-size:11px ">
+		<li><a href="/en/">Copyright &copy; IBWT JD LTD. All rights reserved.</a></li>
+		<li><a href="/en/company/contact">Contact</a></li>		
+		<li><a href="/en/company/aboutus">About</a></li>		
+		<li><a href="/en/company/riskmanagement">Risk</a></li>				
+		<li><a href="/en/company/verification">Verification</a></li>						
+		<li><a href="/en/company/privacy">Privacy</a></li>		
+		<li><a href="/en/company/termsofservice">Terms</a></li>				
+		<li><a href="/en/company/legal">Legal</a></li>		
+		<li><a href="/en/company/FAQ">FAQ</a></li>				
+		<li><a href="/en/company/funding">Funding</a></li>						
+		<li><a href="/en/okpay">OKPAY</a></li>								
+		<li><a href="/en/news">News</a></li>						
+		<li><a href="/en/API">API</a></li>								
+<!--		<li><a href="/en/MAPI">Merchant API</a></li>										-->
+		<li><a href="/en/company/resources">Resources</a></li>								
+	</ul>
+	<p class="nav" style="font-size:11px;border-top:1px solid black ">
+		IBWT is a trading name of <strong>IBWT JD Ltd</strong>. Registered in England and Wales. 
+		Registered office: Plymouth, PL2 2AR. Company no: 8554667. 
+		Registered VAT no: 165221136. 
+		Registered Information Commissioner no: ZA007784.	</p>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-43128731-1', 'ibwt.co.uk');
+  ga('send', 'pageview');
+</script>	
+
+	<script src="/js/main.js?v=84844729"></script>
+	<script src="/bootstrap/js/application.js"></script>
+	<script src="/bootstrap/js/bootstrap-affix.js"></script>
+	<script src="/bootstrap/js/bootstrap-alert.js"></script>
+	<script src="/bootstrap/js/bootstrap-button.js"></script>
+	<script src="/bootstrap/js/bootstrap-carousel.js"></script>
+	<script src="/bootstrap/js/bootstrap-collapse.js"></script>
+	<script src="/bootstrap/js/bootstrap-dropdown.js"></script>
+	<script src="/bootstrap/js/bootstrap-modal.js"></script>
+	<script src="/bootstrap/js/bootstrap-tooltip.js"></script>
+	<script src="/bootstrap/js/bootstrap-popover.js"></script>
+	<script src="/bootstrap/js/bootstrap-scrollspy.js"></script>
+	<script src="/bootstrap/js/bootstrap-tab.js"></script>
+	<script src="/bootstrap/js/bootstrap-transition.js"></script>
+	<script src="/bootstrap/js/bootstrap-typeahead.js"></script>
+	<script src="/bootstrap/js/bootstrap.js"></script>
+	<script src="/bootstrap/js/bootstrap.min.js"></script>
+	
+<script type="text/javascript">
+$(function() {
+	$('.tooltip-x').tooltip();
+  $("input:text:visible:first").focus();
+});
+</script>
+<script>
+</script>
+
 </body>
 </html>
+<?php //echo $this->content(); ?>
